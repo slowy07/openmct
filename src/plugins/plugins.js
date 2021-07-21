da12426da730e4658465db770963fb2911818f16
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2021-07-21 08:50:26 -0700

    implement import export actions to use new form

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -31,7 +31,6 @@ define([
     './timeConductor/plugin',
     '../../example/imagery/plugin',
     './imagery/plugin',
-    '../../platform/import-export/bundle',
     './summaryWidget/plugin',
     './URLIndicatorPlugin/URLIndicatorPlugin',
     './telemetryMean/plugin',
@@ -80,7 +79,6 @@ define([
     TimeConductorPlugin,
     ExampleImagery,
     ImageryPlugin,
-    ImportExport,
     SummaryWidget,
     URLIndicatorPlugin,
     TelemetryMean,
@@ -136,9 +134,6 @@ define([
     plugins.UTCTimeSystem = UTCTimeSystem;
     plugins.LocalTimeSystem = LocalTimeSystem;
     plugins.RemoteClock = RemoteClock.default;
-
-    plugins.ImportExport = ImportExport;
-
     plugins.StaticRootPlugin = StaticRootPlugin;
 
     /**
